aa74beb7dcffa37d33ac4c36d58c9ae2c86b37aa
DonThaKidd <liameevee2008@gmail.com>
2024-10-13 16:32:17 +0100

    Update player.gd

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -15,6 +15,7 @@ func _ready():
 
 func _process(delta):
 	
+	## old direction code
 	# direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	# direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
