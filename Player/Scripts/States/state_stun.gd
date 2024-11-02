669afbed16a2b67d04a98e3c6482a149f374d689
DonThaKidd <liameevee2008@gmail.com>
2024-11-02 14:09:55 +0000

    ep 11

--- Player/Scripts/States/state_stun.gd
+++ Player/Scripts/States/state_stun.gd
@@ -28,13 +28,13 @@ func init() -> void:
 
 # what happens when the player enters this state.
 func Enter() -> void:
-	player.UpdateAnimation("stun")
+	
 	player.animation_player.animation_finished.connect( _animation_finished )
 	
 	direction = player.global_position.direction_to( hurt_box.global_position )
 	player.velocity = direction * -knockback_speed
 	player.SetDirection()
-	
+	player.UpdateAnimation("stun")
 	player.make_invulnerable( invulnerable_duration )
 	player.effect_animation_player.play("damaged")
 	pass
