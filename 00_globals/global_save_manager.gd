13e6c04d6db1dad239fd9b9090cafba6ca6e7653
DonThaKidd <liameevee2008@gmail.com>
2024-11-11 17:16:12 +0000

    ep 15
    not saving the scene path

---
+++ 00_globals/global_save_manager.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+const SAVE_PATH = "user://"
+
+
+signal game_loaded
+signal game_saved
+
+var current_save : Dictionary = {
+	scene_path = "",
+	player = {
+		hp = 1,
+		max_hp = 1, 
+		pos_x = 0,
+		pos_y = 0,
+	},
+	items = [],
+	persistence = [],
+	quests = []
+}
+
+
+func save_game() -> void:
+	update_player_data()
+	update_scene_path()
+	var file := FileAccess.open( SAVE_PATH + "save.sav", FileAccess.WRITE )
+	var save_json = JSON.stringify( current_save )
+	file.store_line( save_json )
+	game_saved.emit()
+	pass
+
+
+func load_game() -> void:
+	print("load game")
+	pass
+
+
+func update_player_data() -> void:
+	var p : Player = PlayerManager.player
+	current_save.player.hp = p.hp 
+	current_save.player.max_hp = p.max_hp
+	current_save.player.pos_x = p.global_position.x
+	current_save.player.pos_y = p.global_position.y
+	
+
+func update_scene_path() -> void:
+	var p : String = ""
+	for c in get_tree().root.get_children():
+		if c is Level:
+			p = c.scene_file_path
+	current_save.scene_path = p
