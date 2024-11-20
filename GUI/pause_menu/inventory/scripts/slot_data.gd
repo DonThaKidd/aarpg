f7bfd9b54a7bcfc9a64df06f8ecb52538e34a9a0
DonThaKidd <liameevee2008@gmail.com>
2024-11-20 18:08:47 +0000

    ep 17 done
    bug: not removing item after reaching 0.

--- GUI/pause_menu/inventory/scripts/slot_data.gd
+++ GUI/pause_menu/inventory/scripts/slot_data.gd
@@ -2,3 +2,8 @@ class_name SlotData extends Resource
 
 @export var item_data : ItemData
 @export var quantity : int = 0
+
+func set_quantity( value : int ) -> void:
+	quantity = value
+	if quantity < 1:
+		emit_changed()
